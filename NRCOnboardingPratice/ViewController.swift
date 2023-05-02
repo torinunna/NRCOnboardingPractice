f33628fab1ab7b1bcba4acb294121b9b91205dc1
torinunna <torinunna9588@gmail.com>
2023-05-02 15:57:57 +0900

    Initial Commit

---
+++ NRCOnboardingPratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NRCOnboardingPratice
+//
+//  Created by YUJIN KWON on 2023/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
