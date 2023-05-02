58a0ae703f8916443cccd961f69700ef66821e36
torinunna <torinunna9588@gmail.com>
2023-05-02 22:02:00 +0900

    create OnboardingViewController and connect to storyboard

---
+++ NRCOnboardingPratice/OnboardingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboardingPratice
+//
+//  Created by YUJIN KWON on 2023/05/02.
+//
+
+import UIKit
+
+class OnboardingViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
