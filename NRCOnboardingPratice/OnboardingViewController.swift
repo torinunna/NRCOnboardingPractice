41aa31afe7becd744b7656492336747f8fa9404a
torinunna <torinunna9588@gmail.com>
2023-05-04 16:24:05 +0900

    make line breaks clear

--- NRCOnboardingPratice/OnboardingViewController.swift
+++ NRCOnboardingPratice/OnboardingViewController.swift
@@ -20,36 +20,31 @@ class OnboardingViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout { layout.estimatedItemSize = .zero
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
         }
         
         pageControl.numberOfPages = messages.count
-        
     }
-    
 }
 
 extension OnboardingViewController: UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return messages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else { return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {
+            return UICollectionViewCell()
         }
         
         let message = messages[indexPath.item]
         cell.configure(message)
-        
         return cell
     }
-    
-    
 }
 
 extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return collectionView.bounds.size
     }
@@ -65,10 +60,8 @@ extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
 }
 
 extension OnboardingViewController: UIScrollViewDelegate {
-    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let index = Int(scrollView.contentOffset.x / self.collectionView.bounds.width)
-        
         pageControl.currentPage = index
     }
 }
